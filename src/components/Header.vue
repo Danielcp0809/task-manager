b6fd8928cdcc86d821808e7a8dba243e92029f1f
Daniel Cordova <jdany0809@hotmail.com>
2022-07-21 00:04:13 -0700

    Button component added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,31 @@
 <template>
-    <header>
-        <h1>{{title}}</h1>
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="blue" />
+  </header>
 </template>
 <script>
+import Button from "./Button.vue";
+
 export default {
-    name: 'Header',
-    props:{
-        title: {
-            type: String,
-            default:'Hello world'
-        }
-    }
-}
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+      default: "Hello world",
+    },
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 <style scoped>
-    /* scoped means that everything inside the styles is a local style */
-    header{
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-        margin-bottom: 20px;
-    }
+/* scoped means that everything inside the styles is a local style */
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-bottom: 20px;
+}
 </style>
